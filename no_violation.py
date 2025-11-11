c1a23e5ba85ac2cfb382ac1d483293c53f5c9a68
saraselwadi-ab <saraselwadiab@gmail.com>
2025-11-11 11:24:00 +0300

    Create no_violation.py

---
+++ no_violation.py
@@ -0,0 +1,9 @@
+"""A simple module that demonstrates clean Python code for Pylint."""
+
+def increment(number: int) -> int:
+    """Return the number incremented by one."""
+    return number + 1
+
+
+if __name__ == "__main__":
+    print(increment(5))
